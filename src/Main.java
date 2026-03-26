219e8e6d506a810b84785db446e1fd62421766ad
thanujadasari <thanujadasari02@gmail.com>
2026-03-26 09:19:35 +0530

    UC1: Add welcome message module

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=================================");
+
+    }
+}
